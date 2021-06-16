004347e26b527d9f913045e0c1ca6c41e766a416
IyadElwy <iyadelwy1@gmail.com>
2021-06-16 15:35:51 +0200

    Finished welcome window and enterName Window

--- src/GUI/Constants.java
+++ src/GUI/Constants.java
@@ -9,13 +9,14 @@ import java.io.File;
 public class Constants {
 
     public static final double GAME_WINDOW_SIZE_X =
-            Screen.getPrimary().getBounds().getMaxX() - 30;
+            Screen.getPrimary().getBounds().getMaxX() - 5;
     public static final double GAME_WINDOW_SIZE_Y =
-            Screen.getPrimary().getBounds().getMaxY() - 80;
+            Screen.getPrimary().getBounds().getMaxY() - 35;
 
     public static Media openingMusic = new Media(new File("src/GUI/Resources" +
             "/openingWindowsSound.mp3").toURI().toString());
-    public static Media clickButton = new Media(new File("src/GUI/Resources/sword_button_click.mp3").toURI().toString());
+    public static Media clickButton = new Media(new File("src/GUI/Resources/" +
+            "sword_button_click.mp3").toURI().toString());
 
     public static void playMusic(Media media) {
         MediaPlayer mediaPlayer = new MediaPlayer(media);
